a594e09aefa5c64414a1ec0cbf3672e1e83f66c9
wangyinhang <40753996+WangBank@users.noreply.github.com>
2018-12-04 16:41:56 +0800

    角色模块的增加和展示列表

--- UI/Controllers/RoleInfoController.cs
+++ UI/Controllers/RoleInfoController.cs
@@ -5,9 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UI.Models;
 
 namespace UI.Controllers
 {
+    [LoginCheckFilter(IsCheck = false)]
     public class RoleInfoController : Controller
     {
         public IRoleInfoService role { get; set; }
